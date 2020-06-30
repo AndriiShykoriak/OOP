69f20122a507d6a52780f227e93bae72c0ff05ea
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-06-30 23:18:06 +0300

    added combined method getArea,getVolume

---
+++ src/main/java/ua/com/basicInterfaces/VolumeAreaMeasurable.java
@@ -0,0 +1,7 @@
+package ua.com.basicInterfaces;
+
+import ua.com.basicInterfaces.AreaMeasurable;
+import ua.com.basicInterfaces.VolumeMeasurable;
+
+public interface VolumeAreaMeasurable extends AreaMeasurable, VolumeMeasurable {
+}
