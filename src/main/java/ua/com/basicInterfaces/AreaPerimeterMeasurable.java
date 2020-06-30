e9330c6cdec012d38bab9b729cd9c8226456495a
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-06-30 23:17:52 +0300

    added combined method getArea,getPerimeter

---
+++ src/main/java/ua/com/basicInterfaces/AreaPerimeterMeasurable.java
@@ -0,0 +1,7 @@
+package ua.com.basicInterfaces;
+
+import ua.com.basicInterfaces.AreaMeasurable;
+import ua.com.basicInterfaces.PerimeterMeasurable;
+
+public interface AreaPerimeterMeasurable extends AreaMeasurable, PerimeterMeasurable {
+}
