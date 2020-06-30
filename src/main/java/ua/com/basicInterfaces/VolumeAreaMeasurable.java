a53cac16768bf584e827a5bd538980f2e7b3da24
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-06-30 23:24:02 +0300

    area+volume

--- src/main/java/ua/com/basicInterfaces/VolumeAreaMeasurable.java
+++ src/main/java/ua/com/basicInterfaces/VolumeAreaMeasurable.java
@@ -1,7 +1,4 @@
 package ua.com.basicInterfaces;
 
-import ua.com.basicInterfaces.AreaMeasurable;
-import ua.com.basicInterfaces.VolumeMeasurable;
-
 public interface VolumeAreaMeasurable extends AreaMeasurable, VolumeMeasurable {
 }
