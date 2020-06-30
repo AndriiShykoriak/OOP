a3329d8d63ec88d3f49188484dca13b2bab599d0
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-06-30 23:23:49 +0300

    area+perimeter

--- src/main/java/ua/com/basicInterfaces/AreaPerimeterMeasurable.java
+++ src/main/java/ua/com/basicInterfaces/AreaPerimeterMeasurable.java
@@ -1,7 +1,4 @@
 package ua.com.basicInterfaces;
 
-import ua.com.basicInterfaces.AreaMeasurable;
-import ua.com.basicInterfaces.PerimeterMeasurable;
-
 public interface AreaPerimeterMeasurable extends AreaMeasurable, PerimeterMeasurable {
 }
